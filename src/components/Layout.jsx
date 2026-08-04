658126097759d29882b5d2731a8e49335cec8388
ibrahim <ibrahim.taseer2012@gmail.com>
2026-08-04 15:26:11 +0500

    Created Layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Layout = ({ children }) => {
+    return (
+        <div className='bg-neutral-800 text-white'>
+            <header className='flex items-baseline justify-between py-12 mx-8 border-b-neutral-400 border-b'>
+                <Link to='/' className='no-underline flex items-center gap-1 font-body'>
+                    <span className='bg-amber-47 w-3 h-3 rounded-full'></span>
+                    <span>Darkroom</span>
+                </Link>
+                <p className='mono'>Your image toolkit</p>
+            </header>
+            <main className='mx-8'>{children}</main>
+            <footer className='flex items-baseline justify-between py-12 mx-8 border-t-neutral-400 border-t'>
+                <p className='mono'>Copyright@2027 Darkroom</p>
+            </footer>
+        </div>
+    )
+}
+
+export default Layout
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import Home from './pages/Home.jsx';
 import ToolPage from './pages/ToolPage.jsx';
+import Layout from './components/Layout.jsx';
 
 const router = createBrowserRouter([{
   path: '/',
-  element: <App />
+  element: <Home />
 }, {
   path: '/tools/:toolId',
   element: <ToolPage />
 }]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />)
