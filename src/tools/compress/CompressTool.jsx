05e78c37a624293833f5dc6b97cef3db34a56ac5
ibrahim <ibrahim.taseer2012@gmail.com>
2026-08-10 11:17:23 +0500

    Added more preview functionality

--- src/tools/compress/CompressTool.jsx
+++ src/tools/compress/CompressTool.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo } from 'react'
 
 const CompressTool = (props) => {
 
-    const files = props.files
+    const files = props.files || []
     const setFiles = props.setFiles
     const [imageUrl, setImageUrl] = useState([])
-    const [quality, setQuality] = useState(70) // default compression quality
+    const [quality, setQuality] = useState(70)
+
+    const previewUrls = useMemo(
+        () => files.map((file) => URL.createObjectURL(file)),
+        [files]
+    )
 
     const handleCompress = () => {
         files.map(async (file) => {
@@ -29,19 +34,28 @@ const CompressTool = (props) => {
     }
 
     return (
-        <div>
-            <input 
-                type="range" 
-                min="1" 
-                max="100" 
-                value={quality} 
-                onChange={(e) => setQuality(Number(e.target.value))} 
-            />
-            <span>Quality: {quality}</span>
-            <button onClick={handleCompress}>Compress</button>
-            {imageUrl.map((elem, idx) => (
-                <img key={idx} src={elem} />
-            ))}
+        <div className='flex'>
+            <div className='w-3/4 bg-black'>
+                {imageUrl.length > 0
+                    ? imageUrl.map((elem, idx) => (
+                        <img key={idx} src={elem} />
+                    ))
+                    : previewUrls.map((elem, idx) => (
+                        <img key={idx} src={elem} />
+                    ))
+                }
+            </div>
+            <div className='w-1/4 bg-amber-47'>
+                <input 
+                    type="range" 
+                    min="10" 
+                    max="100" 
+                    value={quality} 
+                    onChange={(e) => setQuality(Number(e.target.value))} 
+                />
+                <span>Quality: {quality}</span>
+                <button onClick={handleCompress}>Compress</button>
+            </div>
         </div>
     )
 }
