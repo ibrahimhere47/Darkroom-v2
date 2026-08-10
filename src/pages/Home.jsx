7248865cf851f2321ca0c4a0ef16c2649b88719d
ibrahim <ibrahim.taseer2012@gmail.com>
2026-08-10 10:10:04 +0500

    Added responsive grid padding

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,7 +31,7 @@ const Home = () => {
         <Layout>
             <div className='py-10 w-full'>
                 <Hero />
-                <div className='bg-neutral-900 rounded-2xl p-10 border border-neutral-700'>
+                <div className='bg-neutral-900 rounded-2xl p-2 md:p-7 xl:p-10 border border-neutral-700'>
                     <div className='flex flex-wrap gap-4'>
                     {tools.map((tool, idx) => (
                         <div
